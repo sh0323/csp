f4af438bc14767ad513762cabbb32afddf9f1dde
sh0323 <eoenddl1@gmail.com>
2025-10-27 05:22:38 +0900

    feat: Add main loop and menu selection structure

--- CS_Helper/Program.cs
+++ CS_Helper/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("=====================================");
+    Console.WriteLine("  CS 학습 보조 도구");
+    Console.WriteLine("=====================================");
+    Console.WriteLine(" 메뉴를 선택하세요:");
+    Console.WriteLine(" 1. 진법 변환기");
+    Console.WriteLine(" 2. 2의 보수 변환기");
+    Console.WriteLine(" 3. 종료");
+    Console.WriteLine("=====================================");
+
+    // 다음 단계에서 사용자 입력 처리 및 break문 제거 예정
+    Console.ReadKey(); // 잠시 멈춤
+    break;
+}
